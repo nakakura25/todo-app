dfa1c9837d49e0a9c943a8d66904fb37d95e89f8
kazuya.oki <kazuya.oki@nextbeat.net>
2020-06-08 16:39:55 +0900

    DBのユーザー・パスワードを変更

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -11,13 +11,9 @@ import play.api.mvc._
 
 import model.ViewValueHome
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  
+
   def index() = Action { implicit req =>
     val vv = ViewValueHome(
       title  = "Home",
