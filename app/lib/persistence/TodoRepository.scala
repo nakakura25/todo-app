4506ea03316cd816b473cd1437da8e15a01489cd
nakakura25 <satoshi.nakakura@nextbeat.net>
2022-04-13 11:57:59 +0900

    make repository: todo, category

--- app/lib/persistence/TodoRepository.scala
+++ app/lib/persistence/TodoRepository.scala
@@ -1,34 +1,33 @@
 package lib.persistence
 
 import ixias.persistence.SlickRepository
-import lib.model.Category
+import lib.model.Todo
 import slick.jdbc.JdbcProfile
 
-import javax.swing.JDialog
 import scala.concurrent.Future
 
-case class CategoryRepository[P <: JdbcProfile]()(implicit val driver: P)
-  extends SlickRepository[Category.Id, Category, P]
-  with db.SlickResourceProvider[P] {
+case class TodoRepository[P <: JdbcProfile]()(implicit val driver: P)
+  extends SlickRepository[Todo.Id, Todo, P]
+    with db.SlickResourceProvider[P] {
 
   import api._
 
   def list(): Future[Seq[EntityEmbeddedId]] =
-    RunDBAction(CategoryTable, "slave") { _.result }
+    RunDBAction(TodoTable, "slave") { _.result }
 
   override def get(id: Id): Future[Option[EntityEmbeddedId]] =
-    RunDBAction(CategoryTable, "slave") { _
+    RunDBAction(TodoTable, "slave") { _
       .filter(_.id == id)
       .result.headOption
-  }
+    }
 
   override def add(entity: EntityWithNoId): Future[Id] =
-    RunDBAction(CategoryTable) { slick =>
+    RunDBAction(TodoTable) { slick =>
       slick returning slick.map(_.id) += entity.v
-  }
+    }
 
   override def update(entity: EntityEmbeddedId): Future[Option[EntityEmbeddedId]] =
-    RunDBAction(CategoryTable) { slick =>
+    RunDBAction(TodoTable) { slick =>
       val row = slick.filter(_.id === entity.id)
       for {
         old <- row.result.headOption
@@ -40,7 +39,7 @@ case class CategoryRepository[P <: JdbcProfile]()(implicit val driver: P)
     }
 
   override def remove(id: Id): Future[Option[EntityEmbeddedId]] =
-    RunDBAction(CategoryTable) { slick =>
+    RunDBAction(TodoTable) { slick =>
       val row = slick.filter(_.id === id)
       for {
         old <- row.result.headOption
