fe4103a4c6069ac640de78cacf51ce53f4188941
nakakura25 <satoshi.nakakura@nextbeat.net>
2022-04-18 13:29:26 +0900

    index api: to multi exec

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -47,10 +47,13 @@ class HomeController @Inject() (
   )
 
   def index() = Action async { implicit req =>
+    val start = System.currentTimeMillis
     for {
-      todos      <- TodoRepository.list().map(todos => todos.map(_.v))
-      categories <- categoryService.getCategoryMap()
+      (todos, categories) <-
+        TodoRepository.list().map(todos => todos.map(_.v)) zip categoryService
+          .getCategoryMap()
     } yield {
+      println("処理時間： " + (System.currentTimeMillis - start) + " ミリ秒")
       Ok(views.html.Home(vvList, todos, categories, ColorService.getColorMap()))
     }
   }
