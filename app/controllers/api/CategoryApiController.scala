9cd88b9480b376f832d035615405ec9ab9ed4ef5
nakakura25 <satoshi.nakakura@nextbeat.net>
2022-04-21 10:29:54 +0900

    category crud api

--- app/controllers/api/CategoryApiController.scala
+++ app/controllers/api/CategoryApiController.scala
@@ -1,16 +1,7 @@
 package controllers.api
 
-import lib.model.form.{TodoForm, TodoFormData}
-import lib.model.json.{
-  CategoryToJson,
-  Color,
-  ColorToJson,
-  StatusToJson,
-  TodoFromJson,
-  TodoToJson
-}
-import lib.model.{Category, Todo}
-import play.api.data.Form
+import lib.model.Category
+import lib.model.json.{CategoryFromJson, CategoryToJson, Color, ColorToJson}
 import play.api.i18n.I18nSupport
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
@@ -53,15 +44,51 @@ class CategoryApiController @Inject() (
   }
 
   def store() = Action(parse.json) async { implicit request: Request[JsValue] =>
-    ???
+    val category: Category#WithNoId = request.body
+      .validate[CategoryFromJson]
+      .fold(
+        errors => {
+          throw new Exception("json validation errors")
+        },
+        categoryFromJson => {
+          Category.build(
+            categoryFromJson.name,
+            categoryFromJson.slug,
+            categoryFromJson.color
+          )
+        }
+      )
+    for {
+      _ <- CategoryRepository.add(category)
+    } yield NoContent
   }
 
   def update() = Action(parse.json) async {
     implicit request: Request[JsValue] =>
-      ???
+      val category: Category#EmbeddedId =
+        request.body
+          .validate[CategoryFromJson]
+          .fold(
+            errors => {
+              throw new Exception("json validation errors")
+            },
+            categoryFromJson => {
+              Category(
+                Some(Category.Id(categoryFromJson.id)),
+                categoryFromJson.name,
+                categoryFromJson.slug,
+                categoryFromJson.color
+              ).toEmbeddedId
+            }
+          )
+      for {
+        _ <- CategoryRepository.update(category)
+      } yield NoContent
   }
 
   def delete(id: Long) = Action async { implicit request: Request[AnyContent] =>
-    ???
+    for {
+      _ <- CategoryRepository.removeCategory(Category.Id(id))
+    } yield NoContent
   }
 }
